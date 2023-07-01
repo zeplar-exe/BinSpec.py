a3b48ae682ad967bd43569a9a0df446b84f4aa2e
Zander Baker <zanderbaker100@gmail.com>
2023-06-30 22:24:00 -0700

    Initial commit

---
+++ python_module/BinSpec.py/spec.py
@@ -0,0 +1,2 @@
+def SPEC(specType):
+    pass
---
+++ python_module/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = ''
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="BinSpec.py", 
+        version=VERSION,
+        author="",
+        author_email="",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=[],
+        classifiers= []
+)
